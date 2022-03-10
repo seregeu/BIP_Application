8551b8ce5756c8ef1fbf464a35b9d9eae37555e4
kazarart <atichenkov2000@mail.ru>
2022-03-10 13:05:01 +0300

    fix synch

--- app/src/main/java/com/example/bip/data/network/utils/NetworkConstnats.kt
+++ app/src/main/java/com/example/bip/data/network/utils/NetworkConstnats.kt
@@ -1,7 +1,7 @@
 package com.example.bip.data.network.utils
 
 object NetworkConstants {
-    const val BASE_URL = "https://tinkoff-android-fall21.zulipchat.com"
+    const val BASE_URL = "http://10.210.4.143:8080"
     const val AUTHORIZATION = "Authorization"
     const val CONNECTION_TIMEOUT = 15L
     const val READ_TIMEOUT = 5L
